749a1fa8b02fc7934653a6b8883049877b3c7d88
videzere <videzere@gmail.com>
2021-05-12 21:02:11 +0000

    Actualizar GitHud v3

--- exercises/02.2-maping-array-of-objects-to-li/index.js
+++ exercises/02.2-maping-array-of-objects-to-li/index.js
@@ -7,12 +7,12 @@ const planets = ["Mars", "Venus", "Jupiter", "Earth", "Saturn", "Neptune"];
  * 1) Create the mapping function and use it to generate a new array of
  * planets in html called planetsInHTML
  */
+
 const planetsInHTML = planets.map((planet, index) => (
-	<li>
-		key={(planet, index)} {number}
+	<li key={index} className="list-group-item">
+		{planet}
 	</li>
 ));
-return <ul>{planetsInHTML}</ul>;
 
 // 2) add the array planetsInHTML inside the innerHTML of this ul
 const content = <ul className="list-group m-5">{planetsInHTML}</ul>;
